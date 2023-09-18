5f5d0cf21864b5304c7538e6c947fc107ac8d06a
Mus Maxim <musmaxim.musienko@yandex.ru>
2023-09-18 22:01:20 +0300

    Добавлена карточка автомобиля

---
+++ components/index.ts
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import CustomButton from "./CustomButton";
+import NavBar from "./NavBar";
+import Footer from "./Footer";
+import CustomFilter from "./CustomFilter";
+import SearchBar from "./SearchBar";
+import SearchManufacturer from "./SearchManufacturer";
+import CarCard from "./CarCard";
+
+export {
+  Hero,
+  CustomButton,
+  NavBar,
+  Footer,
+  CustomFilter,
+  SearchBar,
+  SearchManufacturer,
+  CarCard,
+};
